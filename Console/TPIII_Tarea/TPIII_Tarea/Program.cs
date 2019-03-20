dca6363878ad808e31d36ad8bfa1275da31b52da
Brian Preciado <duebrian93@gmail.com>
2019-03-20 10:34:38 -0600

    Tareas

---
+++ Console/TPIII_Tarea/TPIII_Tarea/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPIII_Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of bool: {0} bytes", sizeof(bool));
+            Console.WriteLine("Size of byte: {0} bytes", sizeof(byte));
+            Console.WriteLine("Size of char: {0} bytes", sizeof(char));
+            Console.WriteLine("Size of decimal: {0} bytes", sizeof(decimal));
+            Console.WriteLine("Size of double: {0} bytes", sizeof(double));
+            Console.WriteLine("Size of float: {0} bytes", sizeof(float));
+            Console.WriteLine("Size of int: {0} bytes", sizeof(int));
+            Console.WriteLine("Size of long: {0} bytes", sizeof(long));
+            Console.WriteLine("Size of sbyte: {0} bytes", sizeof(sbyte));
+            Console.WriteLine("Size of short: {0} bytes", sizeof(short));
+            Console.WriteLine("Size of uint: {0} bytes", sizeof(uint));
+            Console.WriteLine("Size of ulong: {0} bytes", sizeof(ulong));
+            Console.WriteLine("Size of ushort: {0} bytes \n\n", sizeof(ushort));
+
+        }
+    }
+}
